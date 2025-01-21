5496d61f0138cfd602d50e09c18fdae111a68b42
Paulo Rabelo <paulo.rabelo@IBN.LOCAL>
2025-01-21 17:33:49 -0300

    JogoDoNumero

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10'); // prompt é para o usuário digitar
 
-// se o chute for = ao numeroSecreto mostre... 
+// console.log para verificar o valor de "chute" após a entrada do usuário
+let chute = prompt('Escolha um número entre 1 e 10'); // prompt para o usuário digitar
+console.log('Valor do chute:', chute);
+
+let numeroSecreto = 5;
+
+// console.log para verificar a comparação entre "chute" e "numeroSecreto"
+console.log('Resultado da comparação:', chute == numeroSecreto);
+
 if (chute == numeroSecreto) {
-    alert('Isso ai! Você descobriu o número secreto ' + numeroSecreto); // ou ${numeroSecreto} isso chama acoplamento
-}
-else {
-    alert('Você errou :(')
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+} else {
+    // console.log para verificar o valor de "numeroSecreto" quando o jogador erra
+    console.log('Valor do número secreto:', numeroSecreto);
+    if (chute > numeroSecreto) {
+        alert(`O numero secreto é menor que ${chute}`);
+    }
+    else {
+        alert(`O numero secreto é maior que ${chute}`);
+    }
 }
