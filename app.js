8edca3ba79e2f68c081a88a4747a75dce64909b5
Paulo Rabelo <paulo.rabelo@IBN.LOCAL>
2025-01-23 14:14:03 -0300

    adição do while

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 alert('Boas vindas ao jogo do número secreto');
 
 // console.log para verificar o valor de "chute" após a entrada do usuário
-let chute = prompt('Escolha um número entre 1 e 10'); // prompt para o usuário digitar
+let chute 
 console.log('Valor do chute:', chute);
 
 let numeroSecreto = 5;
@@ -9,15 +9,20 @@ let numeroSecreto = 5;
 // console.log para verificar a comparação entre "chute" e "numeroSecreto"
 console.log('Resultado da comparação:', chute == numeroSecreto);
 
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    // console.log para verificar o valor de "numeroSecreto" quando o jogador erra
-    console.log('Valor do número secreto:', numeroSecreto);
+//enquanto chute não for igual ao numeroSecreto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+    } else {
+        // console.log para verificar o valor de "numeroSecreto" quando o jogador erra
+        console.log('Valor do número secreto:', numeroSecreto);
     if (chute > numeroSecreto) {
         alert(`O numero secreto é menor que ${chute}`);
     }
     else {
         alert(`O numero secreto é maior que ${chute}`);
+    }
     }
 }
