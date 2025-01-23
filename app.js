11ee1b25d9192b385ab2651e8d82a59c3d619150
Paulo Rabelo <paulo.rabelo@IBN.LOCAL>
2025-01-23 17:17:00 -0300

    OperadorTernário

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+        break;
     } else {
         // console.log para verificar o valor de "numeroSecreto" quando o jogador erra
         console.log('Valor do número secreto:', numeroSecreto);
@@ -29,3 +29,15 @@ while (chute != numeroSecreto) {
         tentativas ++; 
     }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa' //operador ternário mesma coisa que:
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+/*
+if (tentativas > 1){
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+}
+else {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+}
+*/
